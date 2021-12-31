5e67351d3a15e3abd55fb3c420511c6e95b2bf9a
44232g <yoshifumi.tsuji.biz@gmail.com>
2021-12-31 17:10:09 +0900

    Make group for wgld

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ### Reference
 
 - [WebGL 入門](https://developer.mozilla.org/ja/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL)
+- [wgld.org](https://wgld.org/d/webgl/)
---
+++ wgld/step01.js
@@ -0,0 +1,14 @@
+window.onload = main;
+
+function main() {
+  const canvas = document.querySelector('#canvas');
+  const gl = canvas.getContext('webgl');
+
+  if (gl === null) {
+    alert('WebGL を初期化できません。ブラウザあーまたはマシンがサポートしていない可能性があります。');
+    return;
+  }
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
